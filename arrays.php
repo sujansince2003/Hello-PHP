064387bc60a1d586dbffdd506ab01b05bfa3606b
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-12-19 19:22:24 +0545

    associative array

---
+++ arrays.php
@@ -0,0 +1,9 @@
+<!-- associative array -->
+<?php
+$countries=array("USA"=>"washington","Nepal"=>"Kathmandu","India"=>"new Delhi");
+
+foreach($countries as $key=> $country){
+    echo " {$key} ={$country} <br>";
+}
+
+ ?>
